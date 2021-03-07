01d035d6f4c7f753837735b14a6fc2a9bd7805b2
Saad Ishtiaq <79640186+Saad-Ishtiaq@users.noreply.github.com>
2021-03-07 23:48:21 +0500

    Lecture 4 (2.0)  (YouTube L:8)
    Access Modifiers

---
+++ Lecture 4 (2.0)/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lecture_4__2._0_
+{
+    class Class1
+    {
+        public void Test()
+        {
+            var aObj = new A();
+            var bObj = new B();
+        }
+    }
+}
---
+++ Lecture 4 (2.0)_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Lecture_4__2._0_;
+
+namespace Lecture_4__2._0__2
+{
+    class Driver : A
+    {
+
+        public void Test()
+        {
+            var aObj = new A();
+            var bObj = new B();
+            var dObj = new Driver();
+
+            aObj.xInternal = 12;
+            bObj.xInternal = 23;
+
+            aObj.xPI = 12;
+            bObj.xPI = 23;
+            dObj.xPI = 23;
+
+            aObj.xPP = 12;
+            bObj.xPP = 23;
+            dObj.xPP = 12;
+
+            aObj.xProtected = 12;
+            bObj.xProtected = 23;
+            dObj.xProtected = 12;
+        }
+    }
+}
