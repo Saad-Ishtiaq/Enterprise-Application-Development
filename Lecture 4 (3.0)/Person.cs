398493ca717b1067dd0a74c09654a2b73c9fd5d3
Saad Ishtiaq <79640186+Saad-Ishtiaq@users.noreply.github.com>
2021-03-08 00:12:58 +0500

    Lecture 4 (3.0)
    C# Classes

---
+++ Lecture 4 (3.0)/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lecture_9
+{
+    public class Person : System.Object
+    {
+        public string Name;
+        public int Age;
+        public static string Country = "Pakistan";
+        public const string HomePlanet = "Earth";
+        public readonly string HomePlanet2;
+
+        public Person()
+        {
+            Name = default;
+            Age = default;
+            HomePlanet2 = "Moon";
+        }
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+            HomePlanet2 = "Moon";
+
+        }
+    }
+}
