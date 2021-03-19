06fb02fdb7714ac571c32f7678f1f9a20782a97b
Saad-Ishtiaq <bsef18a025@pucit.edu.pk>
2021-03-20 03:53:24 +0500

    Lecture 7 EMS (Youtube L:17,18)

---
+++ Lecture7-EmployeeManagementSystem/EMS_BO/EmployeeBO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EMS_BO
+{
+    public class EmployeeBO
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public decimal Salary { get; set; }
+
+    }
+}
