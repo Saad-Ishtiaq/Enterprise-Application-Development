0b8afc539fc3099c0e5823ace843bc93e66d71f2
Saad Ishtiaq <79640186+Saad-Ishtiaq@users.noreply.github.com>
2021-04-04 19:55:25 +0500

    (Yout ube L:19 20 21 22 23

---
+++ Lecture 8/Delegates/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/*----------------------------Lecture 19--------------------------------------*/
+
+namespace Delegates
+{
+    internal class Calculator
+    {
+        static internal int Add(int a, int b)
+        {
+            int result = a + b;
+            Console.WriteLine($"Add Method result:{result}");
+            return result;
+        }
+        static internal int Subtract(int a, int b)
+        {
+            int result = a - b;
+            Console.WriteLine($"Subtract Method result: {result}");
+            return result;
+        }
+        static internal int Multiply(int a, int b)
+        {
+            int result = a * b;
+            Console.WriteLine($"Multiply Method result: {result}");
+            return result;
+
+        }
+    }
+}
